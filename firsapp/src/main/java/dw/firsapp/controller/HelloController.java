1f2f8182e4d06cba98016d49d60b44d97dd7e0a7
bobo459 <awd458@naver.com>
2024-04-16 17:29:12 +0900

    v0416

--- firsapp/src/main/java/dw/firsapp/controller/HelloController.java
+++ firsapp/src/main/java/dw/firsapp/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/sayhello") //url (주소) 이구나~~ GetMapping: 누구와 누굴 연결해주었구나~~
     public String hello(){
-        return  "Hello World!!";
-    }  //누군가가 이걸 호출해 줬구나~~
-    
+        return  "Hello World!!";//누군가가 이걸 호출해 줬구나~~
+    }
+
 }
