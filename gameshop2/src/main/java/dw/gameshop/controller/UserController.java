772cf78bba4cb11ab1c938fee3f4ec4de840d7ba
bobo459 <awd458@naver.com>
2024-05-28 18:33:52 +0900

    v0528

--- gameshop2/src/main/java/dw/gameshop/controller/UserController.java
+++ gameshop2/src/main/java/dw/gameshop/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -36,11 +37,20 @@ public class UserController {
     }
 
     @PostMapping("login")  //viewcontroller에서의 login과는 다른친구 같으면 에러가 난다.
-    public ResponseEntity<String> login(@RequestBody UserDto userDto) {  //스프링 시큐리티에서 일을 시키고 있어서 앞에서 일을 한다. 대부분 이렇게 사용.빨리 처리해야해서
+    public ResponseEntity<String> login(@RequestBody UserDto userDto, //스프링 시큐리티에서 일을 시키고 있어서 앞에서 일을 한다. 대부분 이렇게 사용.빨리 처리해야해서
+                                                     HttpServletRequest request) {
         Authentication authentication = authenticationManager.authenticate( //authentication : 받은 토큰을 가지고 authentication을 만들고 유저디테일 받을 정보를 ->유저디테일서비스...가서...뭐지...성공하면 시그니처로 안되면 예외처리를 해준다.
                 new UsernamePasswordAuthenticationToken(userDto.getUserId(), userDto.getPassword())
         );
         SecurityContextHolder.getContext().setAuthentication(authentication);  //성공했을때 시크리티콘테스트에 저장
+
+        //포스트맨에서 localhost:8080/user/current get하면 에러나는데 아래 4줄을 써야 섹션이 자동으로 생성되서 인지되게 만들어준다.
+        //세션 생성
+        HttpSession session = request.getSession(true);  //true : 세션이 없으면 새로 생성
+        //세션에 인증 객체 저장
+        session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY,
+                SecurityContextHolder.getContext());
+
 
         return ResponseEntity.ok("Success");  //실패시 예외처리
     }
